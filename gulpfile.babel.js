9ee3131a3b87e957ceaa283eff8ebf20c632192c
liseen315 <5482611@qq.com>
2017-03-30 11:14:14 +0800

    fix project

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,24 @@
-import gulp from 'gulp'
-import express from 'express'
-import clean from 'gulp-clean'
-import gulpPlumber from 'gulp-plumber'
-import typescript from 'gulp-typescript'
-import notify from 'gulp-notify'
-import replace from 'gulp-replace'
+import gulp from 'gulp';
+import express from 'express';
+import clean from 'gulp-clean';
+import gulpPlumber from 'gulp-plumber';
+import typescript from 'gulp-typescript';
+import notify from 'gulp-notify';
+import replace from 'gulp-replace';
+import open from 'opn';
 
-const tsconfig = require('./tsconfig.json')
+
+const tsconfig = require('./tsconfig.json');
 const paths = {
   tsfile: 'src/**/*.ts',
   html: 'src/**/*.html'
-}
+};
+
+const app = express();
+app.use(express.static('bin'));
+
+const port = 2017;
+const uri = 'http://localhost:' + port;
 
 const tsProject = typescript.createProject('./tsconfig.json');
 
@@ -18,11 +26,11 @@ const cleanGlob = (glob) => {
   return () => {
     return gulp.src(glob, { read: false })
       .pipe(clean({ force: true }));
-  }
-}
+  };
+};
 
-gulp.task('clean-script',cleanGlob(['./bin/**/*.js','./bin/**/*.ts']))
-gulp.task('clean-html',cleanGlob(['./bin/**/*.html']))
+gulp.task('clean-script',cleanGlob(['./bin/**/*.js','./bin/**/*.ts']));
+gulp.task('clean-html',cleanGlob(['./bin/**/*.html']));
 
 gulp.task('transpileTs',['clean-script'],()=>{
   return gulp.src(paths.tsfile)
@@ -31,21 +39,28 @@ gulp.task('transpileTs',['clean-script'],()=>{
   .pipe(replace(/process\.env\.([a-zA-Z_]+)?( |,|;|\))/gi, (envCall, envKey, closer) => {
     return `'${process.env[envKey]}'${closer}`;
   }))
-  .pipe(gulp.dest('./bin/'))
-})
+  .pipe(gulp.dest('./bin/'));
+});
 
 gulp.task('transpileHtml',['clean-html'],()=>{
   return gulp.src(paths.html)
-  .pipe(gulp.dest('./bin/'))
-})
+  .pipe(gulp.dest('./bin/'));
+});
 
-gulp.task('build',['transpileTs','transpileHtml'])
+gulp.task('build',['transpileTs','transpileHtml']);
 
 gulp.task('watch',['build'],()=>{
-  gulp.watch(paths.tsfile,['transpileTs'])
-  gulp.watch(paths.html,['transpileHtml'])
-})
+  gulp.watch(paths.tsfile,['transpileTs']);
+  gulp.watch(paths.html,['transpileHtml']);
+});
 
 gulp.task('server',['watch'],()=>{
   //启动服务器
-})
+  app.listen(port,(err)=> {
+    if(err){
+      console.log(err);
+      return;
+    }
+    open(uri);
+  });
+});
