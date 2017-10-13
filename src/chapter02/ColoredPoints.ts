845c70d8ff1f70389e8632e94391d4d8980a64c1
liseen315 <5482611@qq.com>
2017-10-13 14:31:58 +0800

    添加带颜色的点

--- src/chapter02/ColoredPoints.ts
+++ src/chapter02/ColoredPoints.ts
@@ -7,13 +7,14 @@ function main(): void {
     ' gl_Position = a_position;\n' +
     ' gl_PointSize = 10.0;\n' +
     '}\n'
-
   let FSHADER_SOURCE: string =
+    'precision mediump float;\n' +
+    'uniform vec4 u_FragColor;\n' +  
     'void main() {\n' +
-    '  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n' +
+    '  gl_FragColor = u_FragColor;\n' +
     '}\n'
-
-  let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('webgl')
+  
+  let canvas:HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('webgl')
   let gl: WebGLRenderingContext = CuonUtils.getWebGLContext(canvas)
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL')
@@ -28,34 +29,45 @@ function main(): void {
     return
   }
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0)
-  gl.clear(gl.COLOR_BUFFER_BIT)
+  let u_FragColor:WebGLUniformLocation = gl.getUniformLocation(program,'u_FragColor')
+  if (!u_FragColor) {
+    console.log('Failed to get the storage location of u_FragColor')
+    return
+  }
 
-  let g_points: Array<number> = []
-
-  canvas.addEventListener('click', (e: MouseEvent) => {
+ 
+  let g_points: Array<any> = []
+  let g_colors: Array<any> = []
+  canvas.addEventListener('click',(e:MouseEvent) => {
     let x: number = e.clientX
     let y: number = e.clientY
     let targetCanvas: HTMLCanvasElement = e.target as HTMLCanvasElement
     let rect: ClientRect = targetCanvas.getBoundingClientRect()
-    // (x - rect.left) 先将浏览器点击事件坐标转换成canvas坐标,
-    // ((x - rect.left) - canvas.width / 2) 将canvas平移到webgl中心
-    // /(canvas.width / 2)系数转换成webgl
     x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2)
     y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2)
-    g_points.push(x)
-    g_points.push(y)
+    g_points.push([x,y])
+    if (x >= 0.0 && y >= 0.0) {
+      g_colors.push([1.0, 0.0, 0.0, 1.0])
+    }else if (x < 0.0 && y < 0.0) {
+      g_colors.push([0.0, 1.0, 0.0, 1.0])
+    }else {
+      g_colors.push([1.0, 1.0, 1.0, 1.0])
+    }
+
     gl.clear(gl.COLOR_BUFFER_BIT)
-    let len = g_points.length
-    for (let i: number = 0; i < len; i += 2) {
-      gl.vertexAttrib3f(position, g_points[i], g_points[i + 1], 0.0)
-      gl.drawArrays(gl.POINTS, 0, 1)
+    let len:number = g_points.length
+    for (let i:number = 0;i<len;i++) {
+      let xy:Array<any> = g_points[i]
+      let rgba:Array<any> = g_colors[i]
+      gl.vertexAttrib3f(position,xy[0],xy[1],0.0)
+      gl.uniform4f(u_FragColor,rgba[0],rgba[1],rgba[2],rgba[3])
+      gl.drawArrays(gl.POINTS,0,1)
     }
   })
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0)
   gl.clear(gl.COLOR_BUFFER_BIT)
+  
 }
-
 
 main()
