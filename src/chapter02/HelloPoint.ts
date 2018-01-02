9230be2c2ed79f1bc8cf7db5e869a4245dd45527
liseen315 <5482611@qq.com>
2018-01-02 21:37:02 +0800

    更改GLSL 语法为字符串模板

--- src/chapter02/HelloPoint.ts
+++ src/chapter02/HelloPoint.ts
@@ -1,15 +1,17 @@
 import CuonUtils from '../lib/CuonUtils'
 
 function main(): void {
-  let VSHADER_SOURCE: string =
-    'void main() {\n' +
-    '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' +  //设置点坐标
-    '  gl_PointSize = 10.0;\n' +                    //设置点大小
-    '}\n'
-  let FSHADER_SOURCE: string =
-    'void main() {\n' +
-    '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + //设置点颜色
-    '}\n'
+  let VSHADER_SOURCE: string = `
+    void main() {
+      gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+      gl_PointSize = 20.0;
+    }
+  `
+  let FSHADER_SOURCE: string = `
+    void main() {
+      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+  `
 
   let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('webgl')
   let gl:WebGLRenderingContext = CuonUtils.getWebGLContext(canvas)
